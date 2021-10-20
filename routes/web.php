b22853474020dfb63938f76ad98ac090f877f938
M513-T\admin <elaine@ncut.edu.tw>
2021-10-20 11:35:38 +0800

    設定 Route 跳轉頁面

--- routes/web.php
+++ routes/web.php
@@ -29,6 +29,10 @@ Route::get('r2',function () {
     return view('welcome');
 });
 
+Route::get('r4',function () {
+    return redirect('r2');
+});
+
 Route::get('hello/{name?}',function ($name = 'Everybody') {
     return 'Hello,'.$name;
 })-> name('hello.index') ;
